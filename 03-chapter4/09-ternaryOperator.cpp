842a15cda4044bd9175703daff08ef2dac1a0377
noaman saleem <noaman2266@gmail.com>
2024-10-29 19:50:14 +0500

    ternary operator

---
+++ 03-chapter4/09-ternaryOperator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter Number : ";
+    cin>>number;
+
+    //cout<<(number % 2 == 0) ? "Even" : "Odd";
+
+    //cout<<((number % 2 == 0) ? "Even" : "Odd");
+
+    //string numberType;
+    //(number % 2 == 0) ? numberType="Even" : numberType="Odd";
+
+    //cout<<numberType;
+
+
+    return 0;
+}
